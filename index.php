8847c7a81a234a289bf797e9f0b4c5f9578ff446
Shawn Lin <acc0752001@gmail.com>
2024-01-22 20:34:48 +0800

    composite

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$apple = array(
+    "jack"=>1,
+    "mike"=>2,
+    "book"=>3,
+
+);
+
+
+
+$data = [ ['ID' => '102'], ['ID' => '103'], ['ID' => '104'] ];
+
+$data[] = ['ID' => '105'];
+var_dump($data);
+?>
